1e6d483874f9a43eab5c66538b456ab98d881d5b
ishwar.rimal@gmail.com <ishwar.rimal@gmail.com>
2023-05-31 22:12:43 +0530

    added comments

--- JS/react-ployfill.js
+++ JS/react-ployfill.js
@@ -6,14 +6,7 @@
 const React = (() => {
   let data = [];
   let index = 0;
-  /**
-   * Custom implementation of useState hook.
-   *
-   * @memberOf React
-   * @function useState
-   * @param {any} val - The initial value of the state.
-   * @return {[any, function]} - An array containing the current state value and a function to update the state.
-   */
+
   function useState(val) {
     let state = data[index] || val;
     const _index = index;
@@ -24,14 +17,6 @@ const React = (() => {
     return [state, setState];
   }
 
-  /**
-   * Custom implementation of useEffect hook.
-   *
-   * @memberOf React
-   * @function useEffect
-   * @param {function} cb - The callback function to be executed when the dependencies change.
-   * @param {Array} dep - An array of dependencies to track changes.
-   */
   function useEffect(cb, dep) {
     const oldDeps = data[index];
     let hasChanged = true;
@@ -42,26 +27,10 @@ const React = (() => {
     hasChanged && cb();
   }
 
-  /**
-   * Custom implementation of useRef hook.
-   *
-   * @memberOf React
-   * @function useRef
-   * @param {any} val - The initial value to be stored in the reference.
-   * @return {Object} - The mutable ref object.
-   */
   function useRef(val) {
     return useState({ current: val })[0];
   }
 
-  /**
-   * Function to render a component and simulate re-rendering.
-   *
-   * @memberOf React
-   * @function render
-   * @param {function} component - The component function to be rendered.
-   * @return {Object} - The rendered component.
-   */
   function render(component) {
     index = 0;
     let c = component();
@@ -73,10 +42,6 @@ const React = (() => {
 
 /**
  * Example component function.
- *
- * @memberOf React
- * @function Component
- * @return {Object} - An object containing the render function, click function, and check function.
  */
 function Component() {
   const [count, setCount] = React.useState(1);
@@ -87,29 +52,10 @@ function Component() {
     console.log(ref.current);
   }, [count]);
   return {
-    /**
-     * Render function of the component.
-     *
-     * @memberOf React.Component
-     * @function render
-     */
     render: () => {
       console.log({ count, text });
     },
-    /**
-     * Click function to increment the count state.
-     *
-     * @memberOf React.Component
-     * @function click
-     */
     click: () => setCount(count + 1),
-    /**
-     * Check function to update the text state.
-     *
-     * @memberOf React.Component
-     * @function check
-     * @param {string} t - The new text value.
-     */
     check: (t) => setText(t),
   };
 }
