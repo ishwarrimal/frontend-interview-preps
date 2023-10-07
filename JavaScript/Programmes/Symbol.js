30a5f05982c20d4ce9355a522feeaf09a45aa67a
Augasty <sayaksengupta140@gmail.com>
2023-10-07 23:33:36 +0530

    updating the 'frontend-interview-preps/JavaScript/Programmes/Symbol.js' file

--- JavaScript/Programmes/Symbol.js
+++ JavaScript/Programmes/Symbol.js
@@ -1,16 +1,17 @@
-const _password = Symbol("password");
 class User {
-  constructor(unsername, password) {
-    this.username = this.username;
-    this[_password] = password;
-  }
+  constructor(username, password) {
+  this.username = username;
+  let _password = Symbol(password)
 
-  getPassword() {
-    return this[_password];
+
+  this.getPassword = () => {
+    return _password;
   }
+}
+
 }
 
 const user = new User("Ishwar", "s3cr3tP@ssw0rd");
 console.log(user.username); // Output: "JohnDoe"
 console.log(user.getPassword()); // Output: "s3cr3tP@ssw0rd"
-console.log(user[_password]); // Output: undefined (Symbol is not accessible without a reference)
+console.log(user._password); // undefined
