8b5973f23927b449c8a36539eeb0446d9a74cf73
Augasty <sayaksengupta140@gmail.com>
2023-10-07 23:34:41 +0530

    updating the 'frontend-interview-preps/JavaScript/Programmes/Symbol.js' file

--- JavaScript/Programmes/Symbol.js
+++ JavaScript/Programmes/Symbol.js
@@ -13,5 +13,5 @@ class User {
 
 const user = new User("Ishwar", "s3cr3tP@ssw0rd");
 console.log(user.username); // Output: "Ishwar"
-console.log(user.getPassword()); // Output: "s3cr3tP@ssw0rd"
+console.log(user.getPassword()); // Output: "Symbol(s3cr3tP@ssw0rd)"
 console.log(user._password); // undefined
