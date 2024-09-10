2307b880847c39ce39fcdedd8639bfa57b882697
Ish <ishwar.rimal@gmail.com>
2024-09-10 15:33:02 +0530

    Update trickythis.js

--- JavaScript/Programmes/trickythis.js
+++ JavaScript/Programmes/trickythis.js
@@ -1,19 +1,28 @@
-var length = 4;
-
 function callback() {
-  console.log(4, this);
+  console.log(2, this);
 }
 
 const object = {
   length: 5,
   method() {
-    console.log(10, this);
+    console.log(8, this);
     arguments[0]();
   },
 };
 
-// The first console.log(this) inside method() will log the object itself because this refers to the object on which the method is called. So, it will output the object and its properties.
+object.method(callback);
 
-// The second console.log(this) inside callback() is a regular function, and this within a regular function refers to the context in which the function is called. In this case, the context is the arguments object since callback is invoked through arguments[0](). Therefore, it will log the arguments object to the console.
+// Output
+8 {length: 5, method: ƒ}
+2 Arguments [ƒ, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+
+// What's happening here?
+
+// The first console.log(this) inside method() will log the object itself because this refers to the object on which the method is called. 
+// So, it will output the object and its properties.
+
+// The second console.log(this) inside callback() is a regular function, and this within a regular function refers to the context in which the function is called. 
+// In this case, the context is the arguments object since callback is invoked through arguments[0](), `this` for callback becomes arguments instead of the object.
 
 // When arguments[0]() is called, it executes the callback function. Since callback is invoked without any explicit context, this within callback will refer to the arguments object. Therefore, it will log the arguments object to the console.
